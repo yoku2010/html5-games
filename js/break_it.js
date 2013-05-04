1a70fdcfdd2d1e9ba382b5727a4b37db66ff5c03
yoku2010 <yoku2010@gmail.com>
2013-05-05 00:13:50 +0530

    Break It Game

---
+++ js/break_it.js
@@ -0,0 +1,12 @@
+/*
+===========================================================================
+* HTML5 Game - Break It
+* Created by - Yogesh Kumar
+* Version - 1.0
+* Date - 01-April-2013
+* browser - IE9+, FF3.6+,Chrome10+
+* Website - http://www.bluejson.com
+* Requires jquery-1.9.0.js or higher
+* Copyright (c) 2013 Yogesh Kumar (http://www.bluejson.com)
+===========================================================================
+*/
--- js/shady.js
+++ js/shady.js
@@ -7,7 +7,7 @@
 * browser - IE9+, FF3.6+,Chrome10+
 * Website - http://www.bluejson.com
 * Requires jquery-1.9.0.js or higher
-* Copyright (c) 2012 Yogesh Kumar (http://www.bluejson.com)
+* Copyright (c) 2013 Yogesh Kumar (http://www.bluejson.com)
 ===========================================================================
 */
 
